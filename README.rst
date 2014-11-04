f8622fcfeae308b0620a061fabe6ac747f908877
Tim Zehta <tim@clockwork.net>
2014-11-03 20:18:17 -0600

    initial commit

---
+++ README.rst
@@ -0,0 +1,22 @@
+Install Formulas
+================
+
+1. `Install Homebrew`_ — The missing package manager for OS X
+2. Add this "tap" and install ``cmc``::
+
+    brew tap TimZehta/tap
+
+3. Install formulas. For example::
+
+    brew install painted-iterm
+
+.. _`Install Homebrew`: http://brew.sh/#install
+
+
+License
+=======
+
+
+- `<LICENSE>`_ (`MIT License`_)
+
+.. _`MIT License`: http://www.opensource.org/licenses/MIT
---
+++ painted-iterm.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class PaintedIterm < Formula
+  homepage "https://github.com/TimZehta/painted-iterm"
+  url "https://github.com/TimZehta/painted-iterm/archive/1.0.0.tar.gz"
+  sha1 "e4b18ccfb86030a81f9e20cfac0c95abed8b010f"
+
+  def install
+    bin.install Dir["bin/*"]
+    prefix.install "LICENSE", "painted_iterm_include.sh", "README.rst"
+    doc.install Dir["docs/*"]
+    doc.install_symlink prefix/"README.rst"
+  end
+
+end
