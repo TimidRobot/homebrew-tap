4b5df54f7e1abc6b2bfa73238165f9b4ac555025
Tim Zehta <tim@clockwork.net>
2014-11-04 23:29:24 -0600

    updated painted-iterm to 1.1.1

--- painted-iterm.rb
+++ painted-iterm.rb
@@ -2,8 +2,8 @@ require "formula"
 
 class PaintedIterm < Formula
   homepage "https://github.com/TimZehta/painted-iterm"
-  url "https://github.com/TimZehta/painted-iterm/archive/1.1.0.tar.gz"
-  sha1 "82053b1d4bc99210e97e8ec9b9b099896b4ec379"
+  url "https://github.com/TimZehta/painted-iterm/archive/1.1.1.tar.gz"
+  sha1 "e6d9cd8126e4a363c8211c1797095ccf4dd3a557"
 
   def install
     bin.install Dir["bin/*"]
