2913cbed95bd3d44ea68b0815ec4cdcbf0912cfc
Tim Zehta <tim@clockwork.net>
2016-08-01 11:39:19 -0500

    updated to use sha256 instead of sha1

--- painted-iterm.rb
+++ painted-iterm.rb
@@ -3,7 +3,7 @@ require "formula"
 class PaintedIterm < Formula
   homepage "https://github.com/TimZehta/painted-iterm"
   url "https://github.com/TimZehta/painted-iterm/archive/1.1.1.tar.gz"
-  sha1 "e6d9cd8126e4a363c8211c1797095ccf4dd3a557"
+  sha256 "774c98f6e65d527c3bc2ae659bac3767162a011f5e4e217e41dea40dd2f7f0dc"
 
   def install
     bin.install Dir["bin/*"]
