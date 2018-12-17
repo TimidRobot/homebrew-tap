2294c9f607423752374c016e7171a592790adf15
Timid Robot Zehta <timidrobot@zehta.me>
2018-12-17 00:19:54 -0600

    cmc 1.1.0

--- Formula/cmc.rb
+++ Formula/cmc.rb
@@ -2,14 +2,14 @@ require 'formula'
 
 class Cmc < Formula
   homepage 'https://github.com/TimidRobot/cmc'
-  url 'https://github.com/TimidRobot/cmc/archive/1.0.3.tar.gz'
-  sha256 'c9efc0e01dccbe8f3be91eb5e54fb4b2f2a57e9fadd26560b5867b339c5e3750'
+  url 'https://github.com/TimidRobot/cmc/archive/1.1.0.tar.gz'
+  sha256 '98ecc49b2a0512b5c18c774730ce4d4dc918c9d9ce85e7fc14eff76931409260'
 
   def install
     bin.install 'cmc'
     doc.install Dir['docs/*']
-    doc.install_symlink prefix/'README.rst'
-    prefix.install 'LICENSE', 'README.rst'
+    doc.install_symlink prefix/'README.md'
+    prefix.install 'LICENSE', 'README.md'
   end
 
 end
