aab84a0a9723dd0eeecd0364791d624a23173b36
Timid Robot Zehta <timidrobot@zehta.me>
2018-12-16 22:01:34 -0600

    use single quotes for non-interpolated strings. fix? install

--- Formula/solo-agent.rb
+++ Formula/solo-agent.rb
@@ -1,15 +1,14 @@
-require "formula"
+require 'formula'
 
 class SoloAgent < Formula
-  homepage "https://github.com/TimidRobot/solo-agent"
-  url "https://github.com/TimidRobot/solo-agent/archive/1.0.0.tar.gz"
-  sha256 "53c92bd43eb759f505de3ae839e5dffcb87932a62317c37e06fbbe9e8748f47d"
+  homepage 'https://github.com/TimidRobot/solo-agent'
+  url 'https://github.com/TimidRobot/solo-agent/archive/1.0.0.tar.gz'
+  sha256 '53c92bd43eb759f505de3ae839e5dffcb87932a62317c37e06fbbe9e8748f47d'
 
   def install
-    bin.install "solo-agent"
-    prefix.install "LICENSE", "README.md"
-    doc.install_symlink prefix/"README.md"
-    prefix.install "LICENSE", "README.md"
+    bin.install 'solo-agent'
+    doc.install_symlink prefix/'README.md'
+    prefix.install 'LICENSE', 'README.md'
   end
 
 end
