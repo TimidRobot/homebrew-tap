2de9fd9e31d2b2b76d93b0120f68f7f084459551
Timid Robot Zehta <timidrobot@zehta.me>
2020-12-17 07:52:37 -0800

    updated solo-agent to 1.0.1

--- Formula/solo-agent.rb
+++ Formula/solo-agent.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class SoloAgent < Formula
   homepage 'https://github.com/TimidRobot/solo-agent'
-  url 'https://github.com/TimidRobot/solo-agent/archive/1.0.0.tar.gz'
-  sha256 '53c92bd43eb759f505de3ae839e5dffcb87932a62317c37e06fbbe9e8748f47d'
+  url 'https://github.com/TimidRobot/solo-agent/archive/1.0.1.tar.gz'
+  sha256 '08a0dee8565b63ac68c25f60e9937f3ae1c431e4d19362b683ad0d830ea60077'
 
   def install
     bin.install 'solo-agent'
